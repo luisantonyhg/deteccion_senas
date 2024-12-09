1048cdf054863908c5b8c18cd8b287beaa82fab8
luisantonyhg <luisantonyhg@gmail.com>
2024-12-09 18:56:20 -0500

    nochehoy

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:detencion_senas/main.dart'; // Ajusta el nombre del paquete según tu proyecto
 
 void main() {
-  test('Ejemplo de test', () {
-    expect(1 + 1, 2);
+  testWidgets('La app se inicia mostrando el texto Bienvenido', (WidgetTester tester) async {
+    // Construye el widget `MyApp` dentro del entorno de prueba.
+    await tester.pumpWidget(MyApp());
+
+    // Busca un widget que contenga el texto "Bienvenido".
+    expect(find.text('Bienvenido'), findsOneWidget);
   });
 }
